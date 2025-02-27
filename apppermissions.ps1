e30f39cf55659c81c0bbf86a2b88ad832383beac
theonlyharsh <creator.zuni@gmail.com>
2025-02-27 17:13:38 +0000

    changed

--- apppermissions.ps1
+++ apppermissions.ps1
@@ -1,9 +1,10 @@
 Connect-MgGraph -Scopes "AppCatalog.ReadWrite.All","Application.Read.All","DelegatedPermissionGrant.ReadWrite.All"
+$allconsents = Get-MgOauth2PermissionGrant -All
 $enterpriseApps = Get-MgServicePrincipal -All
 $appList = @() 
 foreach ($app in $enterpriseApps) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
 Write-Host "Processing: $($app.DisplayName)"
-                      $consents = Get-MgOauth2PermissionGrant -Filter "ClientId eq '$($app.AppId)'"
+                      $consents = $allconsents | Where-Object { $_.clientId -eq $app.AppId }
                         if ($consents.Count -eq 0){
                           Write-Host "No user consents found for: '$($app.DisplayName)'"
                           $appList += [PSCustomObject]@{
